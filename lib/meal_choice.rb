41d7c1c24b8320cd2507b99bc69dca1f0570e0dc
Victoria Hale <vic.hale74@gmail.com>
2019-09-08 03:32:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,9 @@
 def meal_choice(veg1, veg2, protein = "meat")
   
- #puts "What a nutritious meal!"
- #puts "A plate of #{protein} with #{veg1} and #{veg2}."
+ puts "What a nutritious meal!"
+ puts "A plate of #{protein} with #{veg1} and #{veg2}."
   
-  return puts "What a nutritious meal!"
+  return 
   
   end
  
